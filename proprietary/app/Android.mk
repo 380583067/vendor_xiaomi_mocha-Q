bb4a7493d6d9ff22ee8ba1a00d5d8c184e3ad6bf
Dargons10 <jdamr2p@gmail.com>
2024-04-09 16:39:18 +0200

    Drop omniswitch

--- proprietary/app/Android.mk
+++ proprietary/app/Android.mk
@@ -14,20 +14,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-#Omniswitch
-include $(CLEAR_VARS)
-LOCAL_MODULE := omniswitch
-LOCAL_MODULE_OWNER := xiaoMI
-LOCAL_SRC_FILES := omniswitch/omniswitch.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .$(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_APPS)
-include $(BUILD_PREBUILT)
-
 #Keyboard
 include $(CLEAR_VARS)
 LOCAL_MODULE := SimpleKeyboard
